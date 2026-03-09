43caf5ba7dd097c078fa1a2158dc3c5b9ef04bbd
곽나혁 <pigjaoki0970@gmail.com>
2026-03-09 14:51:32 +0900

    feat: MNIST 통합

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <MnistCanvas />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import MnistCanvas from './components/MnistCanvas.vue'
+</script>
+
+<style>
+/* 브라우저 기본 여백 완전히 제거 */
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f1f5f9; /* 카드(흰색)가 돋보이도록 배경을 살짝 톤다운 */
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
